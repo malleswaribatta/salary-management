d873a0e4b8493a10d4c224b54b0b4f35b4dbc777
malleswaribatta <battamalleswari29@gmail.com>
2026-07-08 17:01:02 +0530

    adds debug logs

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,6 @@
 import { app } from "./src/app.ts";
 
 const port = Number(Deno.env.get("PORT")) || 8002;
-
+console.log("app.routes ----> ", app.routes);
+console.log("app ----> ",app);
 Deno.serve({ port }, app.fetch);
